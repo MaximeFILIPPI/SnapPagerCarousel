4fda008f4e587f937058995f9cb8b09734231cb1
Max <filippi.maxime@gmail.com>
2024-02-23 15:29:44 +0700

    + remove previous fix

--- Sources/SnapPagerCarousel/SnapPager.swift
+++ Sources/SnapPagerCarousel/SnapPager.swift
@@ -82,7 +82,8 @@ public struct SnapPager <Content: View, T: Hashable>: View {
                                     .frame(maxWidth: proxy.size.width - edgesOverlap*2)
                                     .containerRelativeFrame(.horizontal)
                             }
-                            .id(item)
+                            //.id(item)
+                            .id(index)
                             .clipped()
                             .frame(width: proxy.size.width - edgesOverlap*2, alignment: .center)
                             
